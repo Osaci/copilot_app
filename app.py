3321485bed4d10f362c2977dbfbc9ecfc9688509
Osaci <42120344+Osaci@users.noreply.github.com>
2024-10-25 09:36:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,55 +6,49 @@ import logging
 import os
 from copilot import CopilotClient
 
+app = Flask(__name__)
+
 prompt = queue.Queue()
 username = 'miikka.karava@outlook.com'
 password = 'VxDH6a4Q8'
 shadow_element = None  
 client = None
 
-def create_app():
-    app = Flask(__name__)
+@app.route('/')
+def home():
+    try:
+        return render_template('index.html')  # "<body>test</body>" #
+    except Exception as e:
+        print(f'exception in index {str(e)}')
+        return f'exception {str(e)}'
 
-    ini_client()
+@app.route('/send-message', methods=['POST'])
+def send_message():
+    user_message = request.json.get('userMessage')
+    print(user_message)
 
-    @app.route('/')
-    def home():
-        try:
-            return render_template('index.html')  # "<body>test</body>" #
-        except Exception as e:
-            print(f'exception in index {str(e)}')
-            return f'exception {str(e)}'
+    if prompt.empty():
+        return jsonify({'reply': "no message received."}), 400
+    try:
+        prompt.put(user_message)
+        print(f'added user message: {user_message}')
+
+        response = None
+        while response is None:
+            time.sleep(2)
+            if not prompt.empty():
+                prompt_queue = prompt.get()
+                response = client.pilot_message(shadow_element, prompt_queue)
+                print(response)
+        return jsonify({'reply': response})
+
+    except Exception as e:
+        print(f'exception in response {str(e)}')
+        return f'exception {str(e)}'
 
 
-    @app.route('/send-message', methods=['POST'])
-    def send_message():
-        user_message = request.json.get('userMessage')
-        print(user_message)
-    
-
-        if not prompt:
-            return jsonify({'reply': "no message received."}), 400
-        try:
-            prompt.put(user_message)
-            print(f'added user message: {user_message}')
-
-            response = None
-            while response is None:
-                time.sleep(2)
-                if not prompt.empty():
-                    prompt_queue = prompt.get()
-                    response = client.pilot_message(shadow_element, prompt_queue)
-                    print(response)
-            return jsonify({'reply': response})
-                     
-        except Exception as e:
-            print(f'exception in response {str(e)}')
-            return f'exception {str(e)}' 
-     
-    return app  
-
-def ini_client():
-    global client
+if __name__ == '__main__':
     client = CopilotClient(url="https://copilot.microsoft.com", client_name='CopilotClient')#, verbose=True)
     client.launch_browser()
     client.login(shadow_element, username, password)
+    app.run(port=8080, debug=False)
